79b892f67f284616865ca8ab74af340320051d62
= <alec.hoyland@gmail.com>
2018-09-18 17:06:44 -0400

    Add extant files

---
+++ ENG-BE-601/homework1.m
@@ -0,0 +1,82 @@
+% Homework #1
+% Alec Hoyland
+% September 10th, 2018
+
+%% Problem #2
+%% Part A
+disp('PROBLEM #2')
+disp('PART A')
+
+% set up the vectors pointing to the vertices and centroid
+M   = [1 4 1];
+N   = [5 2 1];
+P   = [6 6 1];
+b   = [4 4 1];
+
+bigArea = det([M; N; P])/2;
+
+% the area of a triangle is 1/2 * det(A)
+bPN = [b; P; N];
+disp('The determinant of the matrix which specifies the coordinates of triangle bPN is')
+disp(det(bPN))
+disp('The area of triangle bPN is')
+disp(det(bPN)/2)
+
+bMP = [b; M; P];
+disp('The determinant of the matrix which specifies the coordinates of triangle bMP is')
+disp(det(bMP))
+disp('The area of triangle bMP is')
+disp(det(bMP)/2)
+
+bMN = [b; M; N];
+disp('The determinant of the matrix which specifies the coordinates of triangle bMN is')
+disp(det(bMN))
+disp('The area of triangle bMN is')
+disp(det(bMN)/2)
+
+%% Part B
+disp('PART B')
+
+b   = [4.5 3 1];
+bPN = [b; P; N];
+disp('The determinant of the matrix which specifies the coordinates of triangle bPN is')
+disp(det(bPN))
+disp('The area of triangle bPN is')
+disp(det(bPN)/2)
+
+bMP = [b; M; P];
+disp('The determinant of the matrix which specifies the coordinates of triangle bMP is')
+disp(det(bMP))
+disp('The area of triangle bMP is')
+disp(det(bMP)/2)
+
+bMN = [b; M; N];
+disp('The determinant of the matrix which specifies the coordinates of triangle bMN is')
+disp(det(bMN))
+disp('The area of triangle bMN is')
+disp(det(bMN)/2)
+
+% calculate the percent area of each of the smaller triangles
+c = [1/6 2/3 1/6];
+area = [det(bPN) det(bMP) det(bMN)] / 2 / bigArea;
+disp('The area of the little triangles')
+disp(area)
+disp('which is suspiciously similar to the fractional contributions towards the barycenter')
+disp(c)
+
+%% Problem #3
+
+A = [2 3 7; 1 1 2; 2 2 4];
+% find the determinant of A
+disp('The determinant of A is')
+disp(det(A))
+disp('If the determinant of a matrix is zero, the column vectors that comprise the matrix are linearly dependent; no inverse matrix exists.')
+
+a1  = [2 1 2];
+a2  = [3 1 2];
+a3  = [7 2 4];
+
+disp('A possible zero test vector is [1 -3 1], since a1 + -3a2 + a3 = [0 0 0]')
+a1 + -3 * a2 + a3
+
+disp('The vectors span a plane in 2 dimensions')
---
+++ ENG-BE-601/homework2.m
@@ -0,0 +1,114 @@
+%% Homework #2
+% Alec Hoyland
+% 15:01 2018-09-18
+
+pHeader;
+tic
+
+%% Problem #1
+% Using 1-D finite differences to solve the Laplace (heat) equation.
+% The pseudo-3-D thermal conductances are computed by
+% $G_x = k \frac{h_y h_z}{h_x}$, where $k$ is the thermal conductance, and
+% $h$ is the discretization in the subscripted Cartesian direction.
+% The discrete form of this problem can be solved by finding the solution to
+% $\mathrm{A} \vec{u} = \vec{b}$, where  $\mathrm{A}$ describes the connectivity,
+% $\vec{u}$ is the temperature, and $\vec{b}$ the net flux.
+
+% set up the length vector
+hx                  = 0.1;    % m
+hy                  = 0.01;   % m
+hz                  = 0.01;   % m
+x                   = 0:hx:1; % m
+
+% thermal conductances
+k                   = zeros(size(x)); % W / (m*K)
+k(getIndex(x, [0.0 0.4]))     = 27.6; % uranium
+k(getIndex(x, [0.4+hx 0.7]))  = 205;  % aluminum
+k(getIndex(x, [0.7+hx 1.0]))  = 16;   % stainless steel
+
+% heat transfer coefficients
+h                   = 150;    % W / (m^2 * K)
+
+% ambient temperature
+Tinf                = 298;    % K
+% heat source termperature (for left Dirichlet boundary condition)
+Tsource             = 398;    % K
+
+% pseudo-3-D thermal conductances
+G                   = k * (hy * hz) / hx;
+Ginf                = hy * hz * h;
+[~, materials]      = unique(k, 'stable');
+material_names      = {'uranium', 'aluminum', 'stainless steel'};
+for ii = 1:length(materials)
+  disp(['The pseudo-3-D thermal conductance for ' material_names{ii} ' is: ' num2str(G(materials(ii))) ' W/K.'])
+end
+disp(['The pseudo-3-D thermal conductance for a big-ass fan is: ' num2str(Ginf) ' W/K.'])
+
+% generate the connectivity matrix
+A                   = G .* full(gallery('tridiag', length(x), -1, 2, -1));
+% enforce Neumann boundary conditions
+A(end)              = G(end) + Ginf;
+% generate the value vector
+b                   = zeros(length(x), 1);
+% enforce Dirichlet boundary conditions
+b(1)                = G(1) * Tsource;
+% enforce Neumann boundary conditions
+b(end)              = Ginf * Tinf;
+
+% make the matrices sparse
+A = sparse(A);
+b = sparse(b);
+
+% solve for the temperature
+T = A \ b;
+
+% prepend and append boundary conditions
+Tfinal            = zeros(length(T)+2,1);
+Tfinal(1)         = Tsource;  % Dirichlet boundary conditions
+Tfinal(end)       = Tinf;     % Neumann boundary conditions
+Tfinal(2:end-1)   = full(T);
+
+% plot the result
+figure;
+plot([-hx x x(end)+hx], Tfinal)
+xlabel('length (m)')
+ylabel('temperature (K)')
+title('temperature of an anisotropic metal rod')
+box off
+
+prettyFig()
+if being_published
+  snapnow
+  delete(gcf)
+end
+
+
+%% Version Info
+% The file that generated this document is called:
+disp(mfilename)
+
+
+%%
+% and its md5 hash is:
+Opt.Input = 'file';
+disp(dataHash(strcat(mfilename,'.m'),Opt))
+
+
+% %
+% % This file should be in this commit:
+% [status,m]=unix('git rev-parse HEAD');
+% if ~status
+% 	disp(m)
+% end
+
+t = toc;
+
+
+%%
+% This file has the following external dependencies:
+% showDependencyHash(mfilename);
+
+
+%%
+% This document was built in:
+disp(strcat(oval(t,3),' seconds.'))
