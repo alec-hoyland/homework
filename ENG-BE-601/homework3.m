88fdc512a2148856ddc6aef11dcef2081fbaccf7
= <alec.hoyland@gmail.com>
2018-09-26 17:08:32 -0400

    Add homework #3 and data for it

---
+++ ENG-BE-601/homework3.m
@@ -0,0 +1,42 @@
+%% Homework #3
+% Alec Hoyland
+% 17:05 2018-09-26
+
+pHeader;
+tic
+
+%% Part 1: Choosing the Right Polynomial
+% I would choose the cubic function because the data are non-monotonic and odd.
+
+
+
+
+%% Version Info
+% The file that generated this document is called:
+disp(mfilename)
+
+
+%%
+% and its md5 hash is:
+Opt.Input = 'file';
+disp(dataHash(strcat(mfilename,'.m'),Opt))
+
+
+%%
+% This file should be in this commit:
+[status,m]=unix('git rev-parse HEAD');
+if ~status
+	disp(m)
+end
+
+t = toc;
+
+
+%%
+% This file has the following external dependencies:
+showDependencyHash(mfilename);
+
+
+%%
+% This document was built in:
+disp(strcat(oval(t,3),' seconds.'))
