f1996a17b657a44b00f04f06706acbc46533d884
= <alec.hoyland@gmail.com>
2018-09-26 17:43:21 -0400

    Add new plot and fix broken analysis

--- ENG-BE-601/homework3.m
+++ ENG-BE-601/homework3.m
@@ -8,8 +8,63 @@ tic
 %% Part 1: Choosing the Right Polynomial
 % I would choose the cubic function because the data are non-monotonic and odd.
 
+% load the data
+[x, y] = textread('homework3_data_problem1.txt', '%f%f', 'headerlines', 1);
 
+% compute the least-squares coefficients
+X = zeros(length(x), 4);
+X(:, 1) = 1;
+X(:, 2) = x;
+X(:, 3) = x .* x;
+X(:, 4) = x .* x .* x;
 
+c = (X' * X) \ (X' * y);
+
+disp('The least-squares coefficients:')
+disp(c)
+
+% plot the data, y
+figure('OuterPosition',[0 0 1600 1600],'PaperUnits','points','PaperSize',[1600 1600]);
+plot(x, y, 'o')
+hold on
+xfit = linspace(min(x), max(x), 101);
+yfit = c(1) + c(2) * xfit + c(3) * xfit .* xfit + c(4) * xfit .* xfit .* xfit;
+plot(xfit, yfit);
+xlabel('x-data');
+ylabel('y-data');
+title('some data with some regression')
+legend({'data', 'fit'})
+
+prettyFig()
+
+if being_published
+  snapnow
+  delete(gcf)
+end
+
+%% Part 2: Analyzing your Residuals
+fitfunc = @(x) c(1) + c(2) * x + c(3) * x .* x + c(4) * x .* x .* x;
+r     = fitfunc(x) - y;
+sse   = sum(r .* r);
+
+disp('The sum of the squares error is:')
+disp(sse)
+
+% plot a histogram of the residuals
+figure('OuterPosition',[0 0 1600 1600],'PaperUnits','points','PaperSize',[1600 1600]);
+histogram(r, -1:0.025:1)
+xlim([-1 1])
+ylim([0 10])
+ylabel('# of occurances')
+xlabel('value')
+title('distribution of residuals')
+
+disp('The mean of the residuals is:')
+disp(mean(r))
+disp('The variance of the residuals is')
+disp(var(r))
+disp('The variance times N-1 is')
+disp(var(r) * (length(r) - 1))
 
 %% Version Info
 % The file that generated this document is called:
