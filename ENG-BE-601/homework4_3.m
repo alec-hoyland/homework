893f92e15b14107d191c8c83100aee850ff36e7c
= <alec.hoyland@gmail.com>
2018-10-02 11:24:29 -0400

    Homework #4 Problem 3

---
+++ ENG-BE-601/homework4_3.m
@@ -0,0 +1,37 @@
+%% Homework #4, Problem 3
+% Alec Hoyland
+% 11:24 2018-10-2
+
+pHeader;
+tic
+
+
+%% Version Info
+% The file that generated this document is called:
+disp(mfilename)
+
+
+%%
+% and its md5 hash is:
+Opt.Input = 'file';
+disp(dataHash(strcat(mfilename,'.m'),Opt))
+
+
+%%
+% This file should be in this commit:
+[status,m]=unix('git rev-parse HEAD');
+if ~status
+	disp(m)
+end
+
+t = toc;
+
+
+%%
+% This file has the following external dependencies:
+showDependencyHash(mfilename);
+
+
+%%
+% This document was built in:
+disp(strcat(oval(t,3),' seconds.'))
