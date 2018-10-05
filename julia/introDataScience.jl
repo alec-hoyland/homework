a358d318226fd7257f8601a4900ad16701656903
Alec Hoyland <ahoyland@brandeis.edu>
2018-10-05 16:42:33 -0400

    Data Science

--- julia/introDataScience.jl
+++ julia/introDataScience.jl
@@ -3,3 +3,7 @@
 # download the data
 cd("/home/alec/code/homework/julia/")
 P = download("https://raw.githubusercontent.com/nassarhuda/easy_data/master/programming_languages.csv", "programming_languages.csv")
+
+# read the data
+using CSV
+P, H = CSV.read("programming_languages.csv", header=true)
