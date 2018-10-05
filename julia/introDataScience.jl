8439a7bd85ae405c194fe978b0c1116e430a5f76
Alec Hoyland <ahoyland@brandeis.edu>
2018-10-05 16:36:46 -0400

    Intro Data Science

---
+++ julia/introDataScience.jl
@@ -0,0 +1,5 @@
+## Julia for Data Science
+
+# download the data
+cd("/home/alec/code/homework/julia/")
+P = download("https://raw.githubusercontent.com/nassarhuda/easy_data/master/programming_languages.csv", "programming_languages.csv")
