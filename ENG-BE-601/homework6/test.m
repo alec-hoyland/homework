fb62dfd5c1f3bd65cdb66a8f83d212c3d27e2232
= <alec.hoyland@gmail.com>
2018-10-19 14:13:40 -0400

    Publish test

--- ENG-BE-601/homework6/test.m
+++ ENG-BE-601/homework6/test.m
@@ -1,3 +1,10 @@
+%% Proof that this algorithm actually works
+% Alec Hoyland
+% 14:13 2018-10-19
+
+pHeader;
+tic
+
 % define a simple matrix
 A = [1, 0; 1, 0];
 disp('A simple matrix:')
@@ -17,3 +24,33 @@ gamma = shifted_inverse_power(A, v1, true)
 disp('for test vector v2:')
 disp(v2)
 gamma = shifted_inverse_power(A, v2, true)
+
+%% Version Info
+% The file that generated this document is called:
+disp(mfilename)
+
+
+%%
+% and its md5 hash is:
+Opt.Input = 'file';
+disp(dataHash(strcat(mfilename,'.m'),Opt))
+
+
+%%
+% This file should be in this commit:
+[status,m]=unix('git rev-parse HEAD');
+if ~status
+	disp(m)
+end
+
+t = toc;
+
+
+%%
+% This file has the following external dependencies:
+showDependencyHash(mfilename);
+
+
+%%
+% This document was built in:
+disp(strcat(oval(t,3),' seconds.'))
