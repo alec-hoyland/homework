8a54b79bcd5dc1c64851ddfb04f1ea1694168974
= <alec.hoyland@gmail.com>
2018-10-24 14:43:43 -0400

    Add final

---
+++ ENG-BE-601/final.m
@@ -0,0 +1,38 @@
+%% Take-Home Final
+% Alec Hoyland
+% 14:07 2018-10-24
+
+pHeader;
+tic
+
+
+
+%% Version Info
+% The file that generated this document is called:
+disp(mfilename)
+
+
+%%
+% and its md5 hash is:
+Opt.Input = 'file';
+disp(dataHash(strcat(mfilename,'.m'),Opt))
+
+
+%%
+% This file should be in this commit:
+[status,m]=unix('git rev-parse HEAD');
+if ~status
+	disp(m)
+end
+
+t = toc;
+
+
+%%
+% This file has the following external dependencies:
+showDependencyHash(mfilename);
+
+
+%%
+% This document was built in:
+disp(strcat(oval(t,3),' seconds.'))
