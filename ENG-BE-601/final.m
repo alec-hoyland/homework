c98d727f3781140a3089f821e524e20bd657f6b2
= <alec.hoyland@gmail.com>
2018-10-24 14:59:42 -0400

    Plot the singular values

--- ENG-BE-601/final.m
+++ ENG-BE-601/final.m
@@ -5,7 +5,31 @@
 pHeader;
 tic
 
+%% Problem #1
 
+% load the image file and convert into floating point decimals
+img 			= single(imread('/home/ahoyland/code/homework/ENG-BE-601/RedPill2_BW.tif'));
+% invert the tone by making black = 127.5 and white = -127.5
+img 			= - (img - 127.5);
+
+% perform singular-value decomposition
+[U, S, V] = svd(img);
+
+% plot the singular values S on the y-axis vs. a dummy index
+figure('OuterPosition',[0 0 1600 1600],'PaperUnits','points','PaperSize',[1600 1600]);
+sigma 		= S(find(S));
+semilogy(1:length(sigma), sigma);
+grid on
+ylabel('singular values')
+xlabel('nth singular value')
+title('singular values of redpill2')
+
+prettyFig()
+
+if being_published
+	snapnow
+	delete(gcf)
+end
 
 %% Version Info
 % The file that generated this document is called:
