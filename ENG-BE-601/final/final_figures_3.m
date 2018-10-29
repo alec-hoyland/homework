0ff23cdd0e3bb741e60e79c70ceafd16f929af71
= <alec.hoyland@gmail.com>
2018-10-29 16:33:01 -0400

    Make figures

--- ENG-BE-601/final/final_figures_3.m
+++ ENG-BE-601/final/final_figures_3.m
@@ -32,24 +32,51 @@ h 							= pcolor(Xmesh, Ymesh, Xpcolor);
 ch							= colorbar;
 caxis([0 50])
 set(get(ch, 'YLabel'), 'String', '% absorbance')
-set(gca, 'EdgeColor', 'none', 'YDir', 'reverse')
+set(gca, 'YDir', 'reverse')
+set(h, 'EdgeColor', 'none')
 title('Original Data X')
-set(h, 'XTick', x-0.5, 'XTickLabels', myXTickLabels);
+set(gca, 'XTick', (2:11)-0.5, 'XTickLabels', myXTickLabels);
 xlabel('coffee bean bag/type')
 ylabel('wavenumber (cm^{-1})')
 colormap(jet)
-return
 
-% plot the first 3 SVD spectra using pcolor
-for ii = 1:3
-end
-
-figure('OuterPosition',[0 0 1600 1600],'PaperUnits','points','PaperSize',[1600 1600]);
-A 			= spectralSums(100, U, S, V);
-imshow(mat2gray(A));
-title('83.28% compression')
 prettyFig
 drawnow
-saveas(gcf, '/home/ahoyland/code/homework/ENG-BE-601/final/morpheus.jpg')
-saveas(gcf, '/home/ahoyland/code/homework/ENG-BE-601/final/morpheus.png')
+saveas(gcf, '/home/ahoyland/code/homework/ENG-BE-601/final/pcolor_X.jpg')
+saveas(gcf, '/home/ahoyland/code/homework/ENG-BE-601/final/pcolor_X.png')
 close
+
+% plot the first 3 SVD spectra using pcolor
+myCAxis = {[0 50], [0 10], [0 10]};
+
+for ii = 1:3
+	% compute the SVD submatrix
+	A 							= U(:, ii) * S(ii, ii) * V(:, ii)';
+	[nrows, ncols] 	= size(A);
+	fake_column 		= NaN .* ones(nrows, 1);
+	fake_row 				= NaN .* ones(1, ncols + 1);
+	Apcolor 				= [A, fake_column];
+	Apcolor 				= [Apcolor; fake_row];
+	x 							= 1:size(Apcolor, 2);
+	y 							= linspace(900, 1900, size(Apcolor, 1));
+	[Xmesh, Ymesh] 	= meshgrid(x, y);
+	myXTickLabels 	= {'a1', 'a2', 'a3', 'a4', 'a5', 'r1', 'r2', 'r3', 'r4', 'r5'};
+
+	figure('OuterPosition',[0 0 1600 1600],'PaperUnits','points','PaperSize',[1600 1600]);
+	h 							= pcolor(Xmesh, Ymesh, Apcolor);
+	ch							= colorbar;
+	caxis(myCAxis{ii})
+	set(get(ch, 'YLabel'), 'String', '% absorbance')
+	set(gca, 'YDir', 'reverse')
+	set(h, 'EdgeColor', 'none')
+	title(['SVD submatrix ' num2str(ii) ' with \sigma_' num2str(ii) ' = ' num2str(sigma(ii))])
+	set(gca, 'XTick', (2:11)-0.5, 'XTickLabels', myXTickLabels);
+	xlabel('coffee bean bag/type')
+	ylabel('wavenumber (cm^{-1})')
+	colormap(jet)
+
+	prettyFig
+	drawnow
+	saveas(gcf, ['/home/ahoyland/code/homework/ENG-BE-601/final/SVDsubmatrix_' num2str(ii) '.jpg'])
+	saveas(gcf, ['/home/ahoyland/code/homework/ENG-BE-601/final/SVDsubmatrix_' num2str(ii) '.png'])
+end
