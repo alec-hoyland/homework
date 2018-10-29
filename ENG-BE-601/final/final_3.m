45015265579b8903a91d7c1a9307c4cef2a52a80
= <alec.hoyland@gmail.com>
2018-10-29 15:43:18 -0400

    Steps 1-4

--- ENG-BE-601/final/final_3.m
+++ ENG-BE-601/final/final_3.m
@@ -5,10 +5,35 @@
 pHeader;
 tic
 
-[wavenumber, arabica1, arabica2, arabica3, arabica4, arabica5, …
- robusta1, robusta2, robusta3, robusta4, robusta5 ] …
- = textread(‘Problem2_coffee_FTIR_data.txt’, …
- '%f%f%f%f%f %f%f%f%f%f', 'headerlines', 3 )
+%% 1. Load the Data
+[wavenumber, arabica1, arabica2, arabica3, arabica4, arabica5, ...
+ robusta1, robusta2, robusta3, robusta4, robusta5 ] ...
+ = textread('Problem3_coffee_FTIR_data.txt', ...
+ '%f%f%f%f%f%f%f%f%f%f%f', 'headerlines', 3 );
+
+%% 2. Build the Data Matrix
+
+X 				= [arabica1 arabica2 arabica3 arabica4 arabica5 robusta1 robusta2 ...
+					 	robusta3 robusta4 robusta5];
+
+%% Singular Value Decomposition
+
+[U, S, V]	= svd(X);
+
+disp('The eigenvector matrix V:')
+disp(V)
+
+disp('The non-zero singular values:')
+disp(diag(S))
+
+% plot the singular values S on the y-axis vs. a dummy index
+figure('OuterPosition',[0 0 1600 1600],'PaperUnits','points','PaperSize',[1600 1600]);
+sigma 		= S(find(S));
+semilogy(1:length(sigma), sigma);
+grid on
+ylabel('singular values')
+xlabel('nth singular value')
+title('singular values of coffee IR spectra')
 
 %% Version Info
 % The file that generated this document is called:
