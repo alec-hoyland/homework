bfdaf4a400d9e1f784feb35178416e46b44ca941
= <alec.hoyland@gmail.com>
2018-10-29 16:55:23 -0400

    Add 2.1-2.4

--- ENG-BE-601/final/final_3.m
+++ ENG-BE-601/final/final_3.m
@@ -5,18 +5,18 @@
 pHeader;
 tic
 
-%% 1. Load the Data
+%% 1.1 Load the Data
 [wavenumber, arabica1, arabica2, arabica3, arabica4, arabica5, ...
  robusta1, robusta2, robusta3, robusta4, robusta5 ] ...
  = textread('Problem3_coffee_FTIR_data.txt', ...
  '%f%f%f%f%f%f%f%f%f%f%f', 'headerlines', 3 );
 
-%% 2. Build the Data Matrix
+%% 1.2 Build the Data Matrix
 
 X 				= [arabica1 arabica2 arabica3 arabica4 arabica5 robusta1 robusta2 ...
 					 	robusta3 robusta4 robusta5];
 
-%% Singular Value Decomposition
+%% 1.3 Singular Value Decomposition
 
 [U, S, V]	= svd(X);
 
@@ -26,7 +26,7 @@ disp(V)
 disp('The non-zero singular values:')
 disp(diag(S))
 
-% plot the singular values S on the y-axis vs. a dummy index
+%% 1.4 Plot the singular values S on the y-axis vs. a dummy index
 figure('OuterPosition',[0 0 1600 1600],'PaperUnits','points','PaperSize',[1600 1600]);
 sigma 		= S(find(S));
 semilogy(1:length(sigma), sigma);
@@ -35,7 +35,14 @@ ylabel('singular values')
 xlabel('nth singular value')
 title('singular values of coffee IR spectra')
 
-%% SVD Decomposition of the Data
+prettyFig()
+
+if being_published
+	snapnow
+	delete(gcf)
+end
+
+%% 1.5 SVD Decomposition of the Data
 
 %%
 % <</home/ahoyland/code/homework/ENG-BE-601/final/pcolor_X.png>>
@@ -51,6 +58,24 @@ title('singular values of coffee IR spectra')
 %%
 % <</home/ahoyland/code/homework/ENG-BE-601/final/SVDsubmatrix_3.png>>
 
+%% 2.1-2 Choose an SVD Submatrix that Demonstrates Differences Between _C. arabica_ and _C. robusta_
+% I chose the second component since the differences between the species' IR signatures
+% is most pronounced but the intraspecies differences are more minimal.
+
+%% 2.3 Plot Absorbance vs. Wavenumber Spectrum
+
+figure('OuterPosition',[0 0 1600 1600],'PaperUnits','points','PaperSize',[1600 1600]);
+plot(wavenumber, absorbance)
+title(['SVD #2, robusta 2 IR signature'])
+xlabel('wavenumber (cm^{-1})')
+set(gca, 'XDir', 'reverse')
+ylabel('% absorbance')
+grid on
+
+%% 2.4 Chemical Differences in Coffee Species
+% _C. robusta_ has higher chlorogenic acid and caffeine contents on a dry
+% weight basis. These chemicals appear in the ATR spectra and help to discriminate
+% between _C. arabica_ and _C. robusta_.
 
 %% Version Info
 % The file that generated this document is called:
