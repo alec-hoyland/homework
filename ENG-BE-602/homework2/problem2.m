202267526864636c1a3aa386d417e351304764ec
= <alec.hoyland@gmail.com>
2018-11-15 17:40:06 -0500

    problem 2

---
+++ ENG-BE-602/homework2/problem2.m
@@ -0,0 +1,137 @@
+%% Homework #2
+% Problem 2
+% Alec Hoyland
+% 2018-11-15 16:49
+
+pHeader;
+tic
+
+%% 2.1 Constants
+
+a = 1; b = 0.5; c = 3; d = 4;
+
+% dx1/dt = -ax1 + x2
+% dx2/dt = cx1^2 / (x12 + d) - bx2
+
+x1 		= [-10:0.01:10];
+x2 		= x1;
+
+figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+plot(x1, -a*x1, 'k');
+plot(x1, 1/b*c*x1.^2./(x1.^2+d), 'r');
+legend({'x_1''=0', 'x_2''=0'})
+
+xlabel('x_1')
+ylabel('x_2')
+title('phase plot of a dynamical system')
+
+prettyFig()
+
+if being_published
+	snapnow
+	delete(gcf)
+end
+
+%% 2.2 Newton's Method
+
+% dx/dt
+f 			= cell(2,1);
+f{1} 		= @(x1,x2) -a*x1 + x2;
+f{2} 		= @(x1,x2) c*x1^2/(d+x1^2) -b*x2;
+
+% Jacobian
+J 			= cell(2);
+J{1,1}	= @(x1,x2) -a;
+J{1,2}	= @(x1,x2) -a*x1;
+J{2,1}	= @(x1,x2) 2*c*d*x1/(x1^2+d)^2;
+J{2,2}	= @(x1,x2) -b;
+
+% initial conditions
+x0 			= [3.5, 3.7]';
+x(1,1)	= x0(1);
+x(2,1)	= x0(2);
+
+for ii = 2:100
+
+	% evaluate the Jacobian
+	jj 						= zeros(2,2);
+	ff 						= zeros(2,1);
+	for qq = 1:2
+		ff(qq) 		= f{qq}(x(1,ii-1), x(2,ii-1));
+		for ww = 1:2
+			jj(qq,ww) = J{qq,ww}(x(1,ii-1), x(2,ii-1));
+		end
+	end
+
+	% evaluate the new fixed point estimate
+	x(:,ii) 			= x(:,ii-1) - 1 ./ jj * ff;
+
+	% compute the tolerance
+	tol 					= max(abs(x(:,ii) - x(:,ii-1)));
+
+	% display
+	disp(['[INFO] iteration # ' num2str(ii-1)])
+	disp(['[INFO] fixed point estimate: ' mat2str(x(:,ii)')])
+	disp(['[INFO] tolerance: ' num2str(tol)])
+
+	% check for convergence
+	if tol < 0.0001
+		break
+	end
+
+end
+
+x1 		= [-10:0.01:10];
+x2 		= x1;
+
+figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+plot(x1, -a*x1, 'k');
+plot(x1, 1/b*c*x1.^2/(x1.^2+d), 'k');
+
+for ii = 1:size(x, 2)
+	plot(x(1,ii), x(2,ii), 'o')
+	leg{end+1} = ['#' num2str(ii-1)];
+end
+
+xlabel('x_1')
+ylabel('x_2')
+title('phase plot of a dynamical system')
+
+
+prettyFig()
+
+if being_published
+	snapnow
+	delete(gcf)
+end
+
+
+%% Version Info
+% The file that generated this document is called:
+disp(mfilename)
+
+
+%%
+% and its md5 hash is:
+Opt.Input = 'file';
+disp(dataHash(strcat(mfilename,'.m'),Opt))
+
+
+%%
+% This file should be in this commit:
+[status,m]=unix('git rev-parse HEAD');
+if ~status
+	disp(m)
+end
+
+time = toc;
+
+
+%%
+% This file has the following external dependencies:
+showDependencyHash(mfilename);
+
+
+%%
+% This document was built in:
+disp(strcat(oval(time,3),' seconds.'))
