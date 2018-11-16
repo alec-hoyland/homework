7e563ea87ed2f0ae21dc1ad5cd95c3e39b825f51
= <alec.hoyland@gmail.com>
2018-11-16 13:50:50 -0500

    Add part 3

--- ENG-BE-602/homework2/problem2.m
+++ ENG-BE-602/homework2/problem2.m
@@ -17,9 +17,9 @@ x1 		= [-10:0.01:10];
 x2 		= x1;
 
 figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
-plot(x1, -a*x1, 'k');
+plot(x1, a*x1, 'k');
 plot(x1, 1/b*c*x1.^2./(x1.^2+d), 'r');
-legend({'x_1''=0', 'x_2''=0'})
+legend({'x_1''=0', 'x_2''=0'}, 'Location', 'best')
 
 xlabel('x_1')
 ylabel('x_2')
@@ -64,7 +64,7 @@ for ii = 2:100
 	end
 
 	% evaluate the new fixed point estimate
-	x(:,ii) 			= x(:,ii-1) - 1 ./ jj * ff;
+	x(:,ii) 			= x(:,ii-1) - inv(jj) * ff;
 
 	% compute the tolerance
 	tol 					= max(abs(x(:,ii) - x(:,ii-1)));
@@ -85,18 +85,92 @@ x1 		= [-10:0.01:10];
 x2 		= x1;
 
 figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
-plot(x1, -a*x1, 'k');
-plot(x1, 1/b*c*x1.^2/(x1.^2+d), 'k');
-
+plot(x1, a*x1, 'k');
+plot(x1, 1/b*c*x1.^2./(x1.^2+d), 'k');
+leg = cell(size(x,2), 1);
+leg{1} = 'dx_1/dt = 0';
+leg{2} = 'dx_2/dt = 0';
 for ii = 1:size(x, 2)
 	plot(x(1,ii), x(2,ii), 'o')
-	leg{end+1} = ['#' num2str(ii-1)];
+	leg{ii+2} = ['#' num2str(ii)];
 end
+
+legend(leg, 'Location', 'best')
 
 xlabel('x_1')
 ylabel('x_2')
 title('phase plot of a dynamical system')
 
+prettyFig()
+
+if being_published
+	snapnow
+	delete(gcf)
+end
+
+%%
+% If you make a bad guess, the assumption of linear approximations in the Jacobian
+% is not valid. This sometimes works out, but sometimes doesn't. Specifically, if
+% the Jacobian is not positive-definite, which occurs if the Jacobian has complex
+% eigenvalues (as in this case). The jump from $x_0$ to $x_1$ is a good one though.
+
+%% 3.1 Other Root
+clear x
+
+% initial conditions
+x0 			= [1, 7]';
+x(1,1)	= x0(1);
+x(2,1)	= x0(2);
+
+for ii = 2:100
+
+	% evaluate the Jacobian
+	jj 						= zeros(2,2);
+	ff 						= zeros(2,1);
+	for qq = 1:2
+		ff(qq) 		= f{qq}(x(1,ii-1), x(2,ii-1));
+		for ww = 1:2
+			jj(qq,ww) = J{qq,ww}(x(1,ii-1), x(2,ii-1));
+		end
+	end
+
+	% evaluate the new fixed point estimate
+	x(:,ii) 			= x(:,ii-1) - inv(jj) * ff;
+
+	% compute the tolerance
+	tol 					= max(abs(x(:,ii) - x(:,ii-1)));
+
+	% display
+	disp(['[INFO] iteration # ' num2str(ii-1)])
+	disp(['[INFO] fixed point estimate: ' mat2str(x(:,ii)')])
+	disp(['[INFO] tolerance: ' num2str(tol)])
+
+	% check for convergence
+	if tol < 0.0001
+		break
+	end
+
+end
+
+x1 		= [-10:0.01:10];
+x2 		= x1;
+
+figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+plot(x1, a*x1, 'k');
+plot(x1, 1/b*c*x1.^2./(x1.^2+d), 'k');
+leg = cell(size(x,2), 1);
+leg{1} = 'dx_1/dt = 0';
+leg{2} = 'dx_2/dt = 0';
+for ii = 1:size(x, 2)
+	plot(x(1,ii), x(2,ii), 'o')
+	leg{ii+2} = ['#' num2str(ii)];
+end
+
+legend(leg, 'Location', 'best')
+
+xlabel('x_1')
+ylabel('x_2')
+title('phase plot of a dynamical system')
 
 prettyFig()
 
