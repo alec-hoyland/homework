c598c49b2b036ca16e94109a0694e281ce6a354e
Alec Hoyland <ahoyland@brandeis.edu>
2018-11-21 19:06:36 -0500

    Finish alpha-sine function

--- ENG-BE-602/homework3/alpha_sine.m
+++ ENG-BE-602/homework3/alpha_sine.m
@@ -1,10 +1,15 @@
 function [y, J] = alpha_sin(t, alpha, omega)
+  % compute the alpha * sine function
+  % and the associated Jacobian matrix
+  % for parameters alpha and omega
+  % and data t
 
+  % NOTE: alpha is negative and omega is positive (or negative)
 
-  y       = t .* exp(-alpha * t) .* sin(omega * t);
+  y       = t .* exp(alpha * t) .* sin(omega * t);
 
   J       = NaN(2,length(t));
-  J(1,:)  = -alpha * t .* exp(-alpha * t) .* sin(omega * t);
+  J(1,:)  = alpha * t .* exp(alpha * t) .* sin(omega * t);
   J(2,:)  = omega * t .* exp(-alpha * t) .* cos(omega * t);
 
 end % function
