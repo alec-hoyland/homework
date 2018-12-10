57565912da3d3fd1e42b42be2e486fbbb4a096f6
= <alec.hoyland@gmail.com>
2018-12-10 12:37:44 -0500

    Set up template

---
+++ ENG-BE-604/homework5/problem1.m
@@ -0,0 +1,16 @@
+%% Homework #5
+% Problem 1
+% Alec Hoyland
+% 2018-12-10 12:36
+
+pHeader;
+tic
+
+
+%% Version Info
+pFooter;
+time = toc;
+
+%%
+% This document was built in:
+disp(strcat(oval(time,3),' seconds.'))
