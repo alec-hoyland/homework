ddc6474afe1b27d695d9a9d74e15f8b2dfc56718
= <alec.hoyland@gmail.com>
2018-12-17 12:00:15 -0500

    finish

--- ENG-BE-604/final/problem1.m
+++ ENG-BE-604/final/problem1.m
@@ -88,17 +88,51 @@ for ii = 1:100
   % construct a bootstrapped data set
   ys  = yfit - r(bs(:, ii));
   % compute the Hill fit
-  ps(ii, :)  = lsqcurvefit(@hill2, [3, 2.5], S, ys);
+  ps(ii, :)  = lsqcurvefit(@hill2, [p(1,end), p(2,end)], S, ys, [0 0], [4, 4]);
 end
 
 %% 2.4
 
 tbl = table(cell(2,1), NaN(2,1), NaN(2,1), NaN(2, 1));
 tbl.Properties.VariableNames = {'BootstrapStatistic', 'mean', 'stdev', 'ci90pm'};
+tbl.BootstrapStatistic = {'Hill coefficient', 'dissociation constant'}';
 tbl.mean    = mean(ps)';
 tbl.stdev   = std(ps)';
 tbl.ci90pm  = 1.645*tbl.stdev / sqrt(100);
 
+disp(tbl)
+
+%% 2.5
+
+for ii = 1:2
+  figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+  histogram(ps(:, ii))
+  title([num2str(ii-1) '-order parameter'])
+  temp = tbl.mean(ii) + tbl.ci90pm(ii);
+  line([temp, temp], [0 20], 'LineWidth', 2, 'Color', 'r', 'LineStyle', '--');
+  temp = tbl.mean(ii) - tbl.ci90pm(ii);
+  line([temp, temp], [0 20], 'LineWidth', 2, 'Color', 'r', 'LineStyle', '--');
+  xlabel('parameter value')
+  ylabel('# occurring')
+
+  if being_published
+    snapnow
+    delete(gcf)
+  end
+end
+
+%% 2.6
+figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+plot(S, rate, 'o')
+for ii = 1:100
+  plot(S, hill2(ps(ii,:), S), 'LineWidth', 1, 'Color', [0 0 0 0.2]);
+end
+plot(S, yfit, 'k');
+plot(S, hill2(p(1,end)-flip(tbl.ci90pm)', S), '--r');
+plot(S, hill2(p(2,end)+flip(tbl.ci90pm)', S), '--r');
+xlabel('substrate concentration (nM) ')
+ylabel('enzyme rate (rxn/sec)')
+title('Bootstrapped data with Hill function fit and CI')
 
 %% Version Info
 pFooter;
