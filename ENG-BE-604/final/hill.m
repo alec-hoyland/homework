29b048c70858e41b51ff36a75a02fccc92c61862
= <alec.hoyland@gmail.com>
2018-12-17 10:53:40 -0500

    minor fixes

--- ENG-BE-604/final/hill.m
+++ ENG-BE-604/final/hill.m
@@ -5,7 +5,7 @@ function [rate, J] = hill(S, n, k)
   rate    = S.^n ./ (S.^n + k^n);
 
   J       = NaN(2, length(S));
-  J(1,:)  = k^n * s.^n .* (log(S) - log(k)) ./ (s.^n + k^n).^2;
-  J(2,:)  = - n * s.^n * k^(n-1) ./ (s.^n + k^n).^2;
+  J(1,:)  = k^n * S.^n .* (log(S) - log(k)) ./ (S.^n + k^n).^2;
+  J(2,:)  = - n * S.^n * k^(n-1) ./ (S.^n + k^n).^2;
 
 end
