3cb7295e59cebbe1ec7edee74f8d830dd1f15156
= <alec.hoyland@gmail.com>
2018-12-17 10:53:08 -0500

    Add Jacobian

--- ENG-BE-604/final/hill.m
+++ ENG-BE-604/final/hill.m
@@ -1,7 +1,11 @@
-function rate = hill(S, n, k)
+function [rate, J] = hill(S, n, k)
   % computes the hill equation with data S
   % and parameters n and k
 
-  rate = S.^n ./ (S.^n + k^n);
+  rate    = S.^n ./ (S.^n + k^n);
+
+  J       = NaN(2, length(S));
+  J(1,:)  = k^n * s.^n .* (log(S) - log(k)) ./ (s.^n + k^n).^2;
+  J(2,:)  = - n * s.^n * k^(n-1) ./ (s.^n + k^n).^2;
 
 end
