42e0e2c81db06cd1cd594491ad2db018df15df35
= <alec.hoyland@gmail.com>
2018-12-19 17:39:03 -0500

    Finish problem 3b

--- ENG-BE-604/final/logistic_2factor.m
+++ ENG-BE-604/final/logistic_2factor.m
@@ -1,17 +1,23 @@
-function [y, J] = logistic_2factor(beta, x1, x2)
+function [y, J] = logistic_2factor(beta, xdata)
 
-  polynomial = beta(1) + beta(2) * x1 + beta(3) * x2 + ...
-              beta(4) * x1.^2 + beta(5) * x1 .* x2 + beta(6) * x2.^2;
+  % beta is a 1x6 matrix
+
+  polynomial = beta(1) + beta(2) * xdata(:,1) + beta(3) * xdata(:,2) + ...
+              beta(4) * xdata(:,1).^2 + beta(5) * xdata(:,1) .* xdata(:,2) + beta(6) * xdata(:,2).^2;
 
   y = 1 ./ (1 + exp(-polynomial));
 
-  J         = NaN(6, length(x1));
-  kernel    = exp(polynomial) ./ ((exp(polynomial) + 1) .^2);
-  J(1, :)   = kernel;
-  J(2, :)   = x1 .* kernel;
-  J(3, :)   = x2 .* kernel;
-  J(4, :)   = x1 .* x1 .* kernel;
-  J(5, :)   = x1 .* x2 .* kernel;
-  J(6, :)   = x2 .* x2 .* kernel;
+  if nargout > 1
+
+    J         = NaN(length(y), 6);
+    kernel    = exp(polynomial) ./ ((exp(polynomial) + 1) .^2);
+    J(:, 1)   = kernel;
+    J(:, 2)   = xdata(:,1) .* kernel;
+    J(:, 3)   = xdata(:,2) .* kernel;
+    J(:, 4)   = xdata(:,1) .* xdata(:,1) .* kernel;
+    J(:, 5)   = xdata(:,1) .* xdata(:,2) .* kernel;
+    J(:, 6)   = xdata(:,2) .* xdata(:,2) .* kernel;
+
+  end
 
 end
--- ENG-BE-604/final/problem3c.m
+++ ENG-BE-604/final/problem3c.m
@@ -13,9 +13,19 @@ options         = LMFsolve('default');
 options.Display = 1;
 
 % perform parameter estimation
+% estimate initial conditions from the data
+initial         = [73, -14, -10, 1, 0, 1];
+
+% perform parameter estimation
 % betas           = LMFsolve(@(p) norm(y - logistic_2factor(p, x1, x2)), initial, options);
-betas             = LevenbergMarquardt2D([x1 x2], y, @logistic_2factor, 500, [5, 50], [0 0 0 0 0 0], 100);
-betas             = betas(end, :);
+% betas             = LevenbergMarquardt2D([x1 x2], y, @logistic_2factor, 500, [5, 50], [0 0 0 0 0 0], 100);
+options           = optimoptions('lsqcurvefit', ...
+                    'SpecifyObjectiveGradient', true, ...
+                    'Algorithm', 'levenberg-marquardt', ...
+                    'Display', 'iter');
+betas             = lsqcurvefit(@logistic_2factor, initial, [x1 x2], y, [], [], options);
+% betas             = betas(end, :);
+
 % 2. Plot the data in 3-D space
 
 figure;
@@ -31,7 +41,7 @@ for ii = 1:length(xgrid)
     [Z(ii, qq)] = logistic_2factor(betas, xgrid(ii), ygrid(qq));
   end
 end
-surf(X, Y, Z, 'FaceAlpha', 0.2, 'EdgeColor', 'none');
+surf(X, Y, Z', 'FaceAlpha', 0.2, 'EdgeColor', 'none');
 xlabel('factor 1')
 ylabel('factor 2')
 zlabel('cured?')
@@ -45,21 +55,21 @@ if being_published
 end
 
 %% 3. Plot the data in 2-D space
-
-eq = @(x1, x2) betas(1) + betas(2) * x1 + betas(3) * x2 + betas(4) * x1.^2 + betas(5) * x1 .* x2 + betas(6) * x2.^2;
-
-xplot = linspace(-10, 10, 201);
-yplot = NaN(length(xplot), 1);
+xplot = linspace(0, 11, 111);
+yplot = linspace(0, 11, 111);
+cplot = NaN(length(xplot), length(yplot));
 
 for ii = 1:length(xplot)
-  yplot(ii) = fsolve(@(x) eq(xplot(ii), x), 1, optimoptions('fsolve', 'Display', 'off'));
+  for qq = 1:length(yplot)
+    cplot(ii,qq) = logistic_2factor(betas, [xplot(ii), yplot(qq)]);
+  end
 end
 
 figure;
 plot(x1(logical(y)), x2(logical(y)), 'r+');
 hold on
 plot(x1(~logical(y)), x2(~logical(y)), 'ko');
-plot(xplot, yplot, 'k')
+contour(xplot, yplot, cplot', [0.5 0.5]);
 xlabel('factor 1')
 ylabel('factor 2')
 title('2-D logistic_2factor regression')
