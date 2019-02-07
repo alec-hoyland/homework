621cbc55af6322a7a30fe581f845ad0a048b0160
Alec Hoyland <alec.hoyland@posteo.net>
2019-02-07 12:54:15 -0500

    Add Part B

--- ENG-BE-602/homework2/problem2.m
+++ ENG-BE-602/homework2/problem2.m
@@ -15,9 +15,9 @@ k     = [1, 2, 1];
 b     = [0, 1, 0];
 
 % construct the transition matrix
-A     = [0, 0, 1, 0, ...
-         0, 0, 0, 1, ...
-         -(k(1) + k(2))/m(1), k(2)/m(1), -(b(1) + b(2))/m(1), b(2)/m(1), ...
+A     = [0, 0, 1, 0; ...
+         0, 0, 0, 1; ...
+         -(k(1) + k(2))/m(1), k(2)/m(1), -(b(1) + b(2))/m(1), b(2)/m(1); ...
          k(2)/m(2), -(k(2) + k(3))/m(2), b(2)/m(2), -(b(2) + b(3))/m(2)];
 
 % define function for ode solver
@@ -51,7 +51,7 @@ end
 %% 2a.1 Time Series
 
 figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
-plot(t, y);
+plot(t, y(:, 1:2));
 xlabel('time')
 ylabel('displacement')
 legend({'x_1', 'x_2'})
@@ -66,6 +66,186 @@ end
 %% 2a.1 Explanation
 % There are no dissipative forces -- all energy is conserved. It is possible to
 % write a Hamiltonian for this system
+
+%% 2a.2
+
+% constants
+m     = [1, 1];
+k     = [1, 2, 1];
+b     = [0, 20, 0];
+
+% construct the transition matrix
+A     = [0, 0, 1, 0; ...
+         0, 0, 0, 1; ...
+         -(k(1) + k(2))/m(1), k(2)/m(1), -(b(1) + b(2))/m(1), b(2)/m(1); ...
+         k(2)/m(2), -(k(2) + k(3))/m(2), b(2)/m(2), -(b(2) + b(3))/m(2)];
+
+% define function for ode solver
+odefun = @(t, y) A * y;
+
+% initial conditions
+init  = [1, 0, 0, 0];
+
+% time
+tspan = [0, 20*pi];
+
+% solve using RK4 with Dormand-Prince correction
+[t, y] = ode45(odefun, tspan, init);
+
+%% 2a.2 Phase Portrait
+
+figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+plot(y(:, 1), y(:, 2), 'o');
+xlabel('x_1')
+ylabel('x_2')
+axis([-1 1 -1 1])
+axis square
+
+prettyFig()
+
+if being_published
+    snapnow
+    delete(gcf)
+end
+
+%% 2a.2 Time Series
+
+figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+plot(t, y(:, 1:2));
+xlabel('time')
+ylabel('displacement')
+legend({'x_1', 'x_2'})
+
+prettyFig()
+
+if being_published
+    snapnow
+    delete(gcf)
+end
+
+%% 2a.2 Explanation
+% The stiffness increases the time it takes to reach steady-state. A greater proportion
+% of the energy is tied up in the potential of the dashpot initially, though both reach
+% the same amplitude of oscillations at steady-state.
+
+%% 2b.1
+
+% constants
+m     = [1, 1];
+k     = [1, 2, 1];
+b     = [2, 20, 0];
+
+% construct the transition matrix
+A     = [0, 0, 1, 0; ...
+         0, 0, 0, 1; ...
+         -(k(1) + k(2))/m(1), k(2)/m(1), -(b(1) + b(2))/m(1), b(2)/m(1); ...
+         k(2)/m(2), -(k(2) + k(3))/m(2), b(2)/m(2), -(b(2) + b(3))/m(2)];
+
+% define function for ode solver
+odefun = @(t, y) A * y;
+
+% initial conditions
+init  = [1, 0, 0, 0];
+
+% time
+tspan = [0, 20*pi];
+
+% solve using RK4 with Dormand-Prince correction
+[t, y] = ode45(odefun, tspan, init);
+
+%% 2b.1 Phase Portrait
+
+figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+plot(y(:, 1), y(:, 2), 'o');
+xlabel('x_1')
+ylabel('x_2')
+axis([-1 1 -1 1])
+axis square
+
+prettyFig()
+
+if being_published
+ snapnow
+ delete(gcf)
+end
+
+%% 2b.1 Time Series
+
+figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+plot(t, y(:, 1:2));
+xlabel('time')
+ylabel('displacement')
+legend({'x_1', 'x_2'})
+
+prettyFig()
+
+if being_published
+ snapnow
+ delete(gcf)
+end
+
+%% 2b.2 Explanation
+% put words here
+
+%% 2b.2
+
+% constants
+m     = [1, 1];
+k     = [1, 2, 1];
+b     = [2, 20, 0];
+
+% construct the transition matrix
+A     = [0, 0, 1, 0; ...
+         0, 0, 0, 1; ...
+         -(k(1) + k(2))/m(1), k(2)/m(1), -(b(1) + b(2))/m(1), b(2)/m(1); ...
+         k(2)/m(2), -(k(2) + k(3))/m(2), b(2)/m(2), -(b(2) + b(3))/m(2)];
+
+% define function for ode solver
+% non-homeogeneous -- forcing into f_2(t)
+odefun = @(t, y) A * y + [0; 0; 0; -1];
+
+% initial conditions
+init  = [1, 0, 0, 0];
+
+% time
+tspan = [0, 20*pi];
+
+% solve using RK4 with Dormand-Prince correction
+[t, y] = ode45(odefun, tspan, init);
+
+%% 2b.2 Phase Portrait
+
+figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+plot(y(:, 1), y(:, 2), 'o');
+xlabel('x_1')
+ylabel('x_2')
+axis([-1 1 -1 1])
+axis square
+
+prettyFig()
+
+if being_published
+ snapnow
+ delete(gcf)
+end
+
+%% 2b.2 Time Series
+
+figure('OuterPosition',[0 0 1200 1200],'PaperUnits','points','PaperSize',[1200 1200]); hold on
+plot(t, y(:, 1:2));
+xlabel('time')
+ylabel('displacement')
+legend({'x_1', 'x_2'})
+
+prettyFig()
+
+if being_published
+ snapnow
+ delete(gcf)
+end
+
+%% 2b.2 Explanation
+% put words here
 
 %% Version Info
 pFooter;
