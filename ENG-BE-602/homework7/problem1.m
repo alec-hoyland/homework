3eccab3b5a3a54dc54020b1997416d496cdfaccb
Alec Hoyland <alec.hoyland@posteo.net>
2019-03-19 12:58:02 -0400

    Add eqs

--- ENG-BE-602/homework7/problem1.m
+++ ENG-BE-602/homework7/problem1.m
@@ -6,6 +6,11 @@
 pdflib.header;
 tic
 
+%% Boundary Conditions
+% $u \rightarrow 0$ as $z \rightarrow \infty$. $u = 0$ at $r = b= 10, z \neq 0$. Finally, at $z=0, r=b$,
+% $u(r,\phi,0) = 50 - 0.004r^4 \cos(2\phi) - 0.015r^3\sin(\phi)$.
+%
+% $$C_{m,p} = \frac{2}{\phi}b^2[J_{m+1}(k_{m,p}b)]^2} \int_0^{2\pi} h_{cos}(\phi) cos(m\phi) d\phi$$
 
 
 %% Version Info
