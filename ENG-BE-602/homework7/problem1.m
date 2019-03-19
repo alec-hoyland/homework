e65e128fdf4f52afe9c57b82c7467dee96dc25c8
Alec Hoyland <ahoyland@brandeis.edu>
2019-03-19 10:58:46 -0400

    hw7

---
+++ ENG-BE-602/homework7/problem1.m
@@ -0,0 +1,17 @@
+%% Homework #7
+% Problem 1
+% Alec Hoyland
+% 2019-3-16 13:52
+
+pdflib.header;
+tic
+
+
+
+%% Version Info
+pdflib.footer;
+time = toc;
+
+%%
+% This document was built in:
+disp(strcat(strlib.oval(time,3),' seconds.'))
