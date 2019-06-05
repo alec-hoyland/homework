55fe819874c224df966dab8bd4db52ec8e31b519
Alec Hoyland <alec.hoyland@posteo.net>
2019-06-05 16:06:09 -0400

    kNN algorithm in-progress

--- CAS-CS-542/homework2/problem2.jl
+++ CAS-CS-542/homework2/problem2.jl
@@ -51,25 +51,71 @@ function normalize_features!(df)
     return df
 end
 
-function distance(a::AbstractVector{T}, b::AbstractVector{T}) where T <: Number
-    # for numerical vectors, define the L₂ distance
-    d = 0;
-    for ii = 1:length(a)
-        d = d + (a[ii] - b[ii])^2;
+function distance(a, b)
+
+    function point_dist(a::T, b::T) where T <: Number
+        return d = (a-b)^2
+    end
+
+    function point_dist(a::T, b::T) where T <: String
+        if a == b
+            d = 0
+        else
+            d = 1
+        end
+        return d
+    end
+
+    d = 0
+    for i = 1:length(a)
+        d += point_dist(a[i], b[i])
     end
     return sqrt(d)
 end
 
-function distance(a::AbstractVector{T}, b::AbstractVector{T}) where T <: String
-    # for non-numerical vectors, the distance is the square root of the number of mismatches
-    d = 0;
-    for ii = 1:length(a)
-        if a[ii] != b[ii]
-            d = d + 1;
+function majority_vote(labels::Vector)
+    counts = countmap(labels)
+    res = labels[1]
+    max_value = -1
+    for (k, v) in counts
+        if v > max_value
+            res, max_value = k, v
         end
     end
-    return sqrt(d)
+    return res
+end
+
+function predict(training, testing, k=5)
+    # output vector
+    predictedLabels = String[]
+    # compute the distance between each high-dimensional point
+    for i = 1:size(testing, 1)
+        sourcePoint = testing[i, :]
+        distances = Float64[]
+        for j in 1:size(training, 1)-1
+            destPoint = training[j, :]
+            dist = distance(sourcePoint, destPoint)
+            push!(distances, dist)
+        end
+        # sort the candidates
+        candidates = training[sortperm(distances)[1:k]]
+        predictedLabel = skim(candidates)
+        # append the best guess
+        push!(predictedLabels, predictedLabel)
+    end
+    return predictedLabels
 end
 
 df = impute_missing_data!(df);
 df = normalize_features!(df);
+
+function get_k_nearest_neighbors(x::AbstractArray, imageI::AbstractArray, k::Int = 3, train = true)
+    nRows, nCols = size(x)
+    distances = get_all_distances(imageI, x)
+    sortedNeighbors = sortperm(distances)
+    if train
+        return kNearestNeighbors = Array(sortedNeighbors[2:k+1])
+    else
+        return kNearestNeighbors = Array(sortedNeighbors[1:k])
+    end
+end
