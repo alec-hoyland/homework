52896b921c8e96c6ea9a851941ae5aa0579ce6a9
Alec Hoyland <alec.hoyland@posteo.net>
2019-06-17 14:25:53 -0400

    Add files from homework3

---
+++ CAS-CS-542/homework3/homework3.jl
@@ -0,0 +1,13 @@
+#%% ---
+#%% title : Homework 3
+#%% author: Alec Hoyland
+#%% data : 2019-06-17
+#%% options :
+#%%     doctype: md2pdf
+#%% ---
+
+#%% ## Introduction
+#%% We implement a sparse autoencoder, a neural network designed to learn data representations.
+#%% The representations here will be mostly sparse.
+
+cd("/home/alec/code/homework/CAS-CS-542/homework3/")
