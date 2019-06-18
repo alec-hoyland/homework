e95172520c099b0fa6be72fe7b58679be6bd3504
Alec Hoyland <alec.hoyland@posteo.net>
2019-06-18 11:30:10 -0400

    First draft

--- CAS-CS-542/homework3/homework3.jl
+++ CAS-CS-542/homework3/homework3.jl
@@ -16,8 +16,7 @@ cd("/home/alec/code/homework/CAS-CS-542/homework3/")
 
 using NPZ
 using StatsBase
-
-images = npzread("images.npy")
+using NLopt
 
 function sampleImages(images, n=10000; patch_size=8)
     # output matrix
@@ -94,7 +93,7 @@ end
 #%% a neural network, and I wanted to see exactly how the matrices are composed (and decomposed).
 #%% Though I know that the [Flux](https://fluxml.ai/) package in particular has great tools for this.
 
-function autoencoderCost(theta::Vector{T}, visiblesize, hiddensize, data::Array{T, 2}, grad::Vector{T},
+function autoencoder(theta::Vector{T}, visiblesize, hiddensize, data::Array{T, 2}, grad::Vector{T},
             λ::T = 0.0, ρ::T = 0.01, β::T = 0.0) where T <: Real
     # store the parameters
     W1, W2, b1, b2 = unravel(theta, visiblesize, hiddensize)
@@ -134,7 +133,7 @@ function autoencoderCost(theta::Vector{T}, visiblesize, hiddensize, data::Array{
         grad[2*visiblesize*hiddensize+1:2*hiddensize*visiblesize+hiddensize] = (sum(d2,2)/m) |> vec
         grad[2*hiddensize*visiblesize+hiddensize+1:end] = (sum(d3,2)/m) |> vec
     end
-    return cost
+    return cost, grad
 end
 
 #%% The numerical gradient is computed with some small parameter ``\epsilon``
@@ -156,4 +155,134 @@ function computeNumericalGradient(f, x::Vector{T}) where T <: Real
     end
 
     return g
+end
+
+#%% ### Step 3: Testing
+#%% We will test the gradient function to make sure that it's working correctly.
+
+function testGradient()
+
+    # read the data
+    images = npzread("images.npy")
+    traindata = Array(Float64, 64, 100)
+    samples = sampleImages(images)
+
+    # parameters
+    hidden = 25;
+    visible = 64;
+    λ = 1e-4;
+    β = 3.0;
+    ρ = 0.01;
+    theta = 0.1 * randn(2*hidden*visible + hidden + visible)
+
+    function justcost(x)
+        cost, grad = autoencoder(x, visible, hidden, traindata, zeros(size(theta)), λ=λ, ρ=ρ, β=β)
+        return cost
+    end
+
+    # compute the cost and gradient
+    cost, grad = autoencoder(theta, visible, hidden, traindata, zeros(size(theta)), λ=λ, ρ=ρ, β=β)
+    # compute the gradient using the numerical scheme
+    grad_num = computeNumericalGradient(justcost, theta)
+    # compute the normalized difference, 0 => perfect
+    diff = norm(numgrad - grad) / norm(numgrad + grad)^2
+
+    return diff, grad, grad_num
+end
+
+diff, grad, grad_num = testGradient()
+
+#%% In the test of the gradient, we should expect the normalized difference to be close to zero.
+#%% As our parameter ``\epsilon`` decreases, we should see convergence in accuracy.
+#%% Since both of our schemes are numerical here, we just hope for decent accordance between the methods.
+#%% This will tell us that our backpropagation is working.
+
+#%% ### Step 4: Using the algorithm
+
+function initialize(hiddensize, visiblesize)
+    r = sqrt(6) / sqrt(hiddensize+visiblesize+1)
+    W1 = rand(hiddensize,visiblesize)*2*r - r
+    W2 = rand(visiblesize,hiddensize)*2*r - r
+    b1 = zeros(hiddensize)
+    b2 = zeros(visiblesize)
+    return [W1[:],W2[:],b1,b2]
+end
+
+function autoencode(data, hiddensize, visiblesize; λ=1e-4, β=3.0, ρ=0.01, maxiter=400)
+    theta::Vector{Float64} = initializeparameters(hiddensize,visiblesize)
+    count = 0
+
+    function objective(x,grad)
+        count += 1
+        println("iteration $count")
+        return autoencoder(x, visible, hidden, traindata, grad, λ=λ, ρ=ρ, β=β)
+    end
+
+    opt = Opt(:LD_LBFGS,length(theta))
+
+    min_objective!(opt, objective)
+    maxeval!(opt, maxiter)
+    minf, minx, ret = optimize(opt, theta)
+
+    W1 = reshape(minx[1:hiddensize*visiblesize],hiddensize,visiblesize)
+    W2 = reshape(minx[hiddensize*visiblesize+1:2*hiddensize*visiblesize],visiblesize,hiddensize)
+    b1 = minx[2*hiddensize*visiblesize+1:2*hiddensize*visiblesize+hiddensize]
+    b2 = minx[2*hiddensize*visiblesize+hiddensize+1:end]
+
+    return minf, W1, W2, b1, b2
+end
+
+function main()
+    # load the data
+    images = npzread("images.npy")
+    traindata = sampleImages(images)
+
+    # useful parameters
+    hiddensize = 25
+    visiblesize = 64
+    λ = 1e-4
+    β = 3.0
+    ρ = 0.01
+
+    f,W1,W2,b1,b2 = autoencode(traindata,hiddensize,visiblesize)
+    displaynetwork(W1', "hw3-autoencoder")
+end
+
+# The following function was copied from kjchavez on GitHub
+# https://github.com/kjchavez/deep-learning-julia/blob/master/autoencoder/sparse-autoencoder-tutorial.jl#L90
+
+function displaynetwork(A,filename)
+        m,n = size(A)
+        sz = int(sqrt(m))
+        A -= mean(A)
+        layout = [
+            "autosize" => false,
+            "width" => 500,
+            "height"=> 500
+        ]
+
+        gridsize = int(ceil(sqrt(n)))
+        buffer = 1
+        griddata = ones(gridsize*(sz+1)+1,gridsize*(sz+1)+1)
+        index = 1
+        for i = 1:gridsize
+                for j = 1:gridsize
+                        if index > n
+                                continue
+                        end
+                        columnlimit = maximum(abs(A[:,index]))
+                        griddata[buffer+(i-1)*(sz+buffer)+(1:sz),buffer+(j-1)*(sz+buffer)+(1:sz)] = reshape(A[:,index],sz,sz)/columnlimit
+                        index += 1
+                end
+        end
+
+        data = [
+          [
+            "z" => griddata,
+            "colorscale" => "Greys",
+            "type" => "heatmap"
+          ]
+        ]
+        response = Plotly.plot(data, ["layout" => layout, "filename" => filename, "fileopt" => "overwrite"])
+        plot_url = response["url"]
 end
