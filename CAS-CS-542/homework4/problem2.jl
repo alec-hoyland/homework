a885b51502805f57eeb97b70bd3bb409d2f150a0
Alec Hoyland <alec.hoyland@posteo.net>
2019-06-28 12:00:49 -0400

    Add preamble

--- CAS-CS-542/homework4/problem2.jl
+++ CAS-CS-542/homework4/problem2.jl
@@ -1,3 +1,15 @@
+#%% ---
+#%% title : Homework 4
+#%% author: Alec Hoyland
+#%% date : 2019-06-28
+#%% options :
+#%%     doctype: md2pdf
+#%% ---
+
+#%% The algorithm will always converge to a local minimum,
+#%% but there is no guarantee of global convergence.
+#%% 
+
 cd("/home/alec/code/homework/CAS-CS-542/homework4/")
 
 using FileIO, Images
@@ -9,7 +21,7 @@ getPixelValues(img) = unique(gray.(img))
 function findNeighbors(x::AbstractArray{T, 2}, i::Int, j::Int) where T
     sz = size(x) .+ 1
     x_limit = (0, sz[1])
-    y_limit = (0, size[2])
+    y_limit = (0, sz[2])
 
     possible_neighbors = [(i, j+1), (i, j-1), (i+1, j), (i-1, j)]
     neighbors = typeof(possible_neighbors)()
@@ -33,4 +45,16 @@ function energy(x, y; h=0, β=0, η=0)
     # pixel preference term
     E += h * sum(img)
     # neighbor similarity term
-    E += β *
+    sz = size(img)
+    for i in 1:sz[1], j in 1:sz[2]
+        neighbors, testpoint = findNeighbors(img, i, j)
+        for n in neighbors
+            E += β * img[i, j] * img[n...]
+        end
+    end
+    # encourage flipping term
+    for i in 1:sz[1], j in 1:sz[2]
+        E += η * img[i, j] * y[i, j]
+    end
+    return E
+end
