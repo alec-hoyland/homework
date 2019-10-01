004ff1439c6fe39daac00a6e773367607a704311
Alec Hoyland <alec.hoyland@posteo.net>
2019-10-01 12:14:29 -0400

    Use new code

--- CAS-CS-640/programming1/nnet.py
+++ CAS-CS-640/programming1/nnet.py
@@ -1,29 +1,30 @@
-# %% 1
-# Package imports
-import matplotlib.pyplot as plt
 import numpy as np
-import sklearn
-import sklearn.datasets
-import sklearn.linear_model
-import matplotlib
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
 
-# Display plots inline and change default figure size
-# %matplotlib inline
-matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
 
-# # %% 2
-# np.random.seed(3)
-# X, y = sklearn.datasets.make_moons(200, noise=0.20)
-# plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
-#
-# # %% 3
-# # Train the logistic rgeression classifier
-# clf = sklearn.linear_model.LogisticRegressionCV()
-# clf.fit(X, y)
+class Config:
+    nn_input_dim = 2  # input layer dimensionality
+    nn_output_dim = 2  # output layer dimensionality
+    # Gradient descent parameters (I picked these by hand)
+    epsilon = 0.01  # learning rate for gradient descent
+    reg_lambda = 0.01  # regularization strength
 
-# %% 4
-# Helper function to plot a decision boundary.
-def plot_decision_boundary(pred_func):
+
+def generate_data():
+    np.random.seed(0)
+    X, y = datasets.make_moons(200, noise=0.20)
+    return X, y
+
+
+def visualize(X, y, model):
+    # plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
+    # plt.show()
+    plot_decision_boundary(lambda x:predict(model,x), X, y)
+    plt.title("Logistic Regression")
+
+
+def plot_decision_boundary(pred_func, X, y):
     # Set min and max values and give it some padding
     x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
     y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
@@ -36,24 +37,12 @@ def plot_decision_boundary(pred_func):
     # Plot the contour and training examples
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+    plt.show()
 
-# %% 12
-# Plot the decision boundary
-# plot_decision_boundary(lambda x: clf.predict(x))
-# plt.title("Logistic Regression")
 
-# %% 15
-# num_examples = len(X) # training set size
-# nn_input_dim = 2 # input layer dimensionality
-# nn_output_dim = 2 # output layer dimensionality
-#
-# # Gradient descent parameters (I picked these by hand)
-# epsilon = 0.01 # learning rate for gradient descent
-# reg_lambda = 0.01 # regularization strength
-
-# %% 7
 # Helper function to evaluate the total loss on the dataset
-def calculate_loss(model):
+def calculate_loss(model, X, y):
+    num_examples = len(X)  # training set size
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
     # Forward propagation to calculate our predictions
     z1 = X.dot(W1) + b1
@@ -65,11 +54,10 @@ def calculate_loss(model):
     corect_logprobs = -np.log(probs[range(num_examples), y])
     data_loss = np.sum(corect_logprobs)
     # Add regulatization term to loss (optional)
-    data_loss += reg_lambda/2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
-    return 1./num_examples * data_loss
+    data_loss += Config.reg_lambda / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+    return 1. / num_examples * data_loss
 
-# %% 8
-# Helper function to predict an output (0 or 1)
+
 def predict(model, x):
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
     # Forward propagation
@@ -80,19 +68,19 @@ def predict(model, x):
     probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
     return np.argmax(probs, axis=1)
 
-# %% 16
+
 # This function learns parameters for the neural network and returns the model.
 # - nn_hdim: Number of nodes in the hidden layer
 # - num_passes: Number of passes through the training data for gradient descent
 # - print_loss: If True, print the loss every 1000 iterations
-def build_model(nn_hdim, num_passes=20000, print_loss=False):
-
+def build_model(X, y, config, nn_hdim, num_passes=20000, print_loss=False):
     # Initialize the parameters to random values. We need to learn these.
+    num_examples = len(X)
     np.random.seed(0)
-    W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
+    W1 = np.random.randn(config.nn_input_dim, nn_hdim) / np.sqrt(config.nn_input_dim)
     b1 = np.zeros((1, nn_hdim))
-    W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
-    b2 = np.zeros((1, nn_output_dim))
+    W2 = np.random.randn(nn_hdim, config.nn_output_dim) / np.sqrt(nn_hdim)
+    b2 = np.zeros((1, config.nn_output_dim))
 
     # This is what we return at the end
     model = {}
@@ -117,39 +105,40 @@ def build_model(nn_hdim, num_passes=20000, print_loss=False):
         db1 = np.sum(delta2, axis=0)
 
         # Add regularization terms (b1 and b2 don't have regularization terms)
-        dW2 += reg_lambda * W2
-        dW1 += reg_lambda * W1
+        dW2 += config.reg_lambda * W2
+        dW1 += config.reg_lambda * W1
 
         # Gradient descent parameter update
-        W1 += -epsilon * dW1
-        b1 += -epsilon * db1
-        W2 += -epsilon * dW2
-        b2 += -epsilon * db2
+        W1 += -config.epsilon * dW1
+        b1 += -config.epsilon * db1
+        W2 += -config.epsilon * dW2
+        b2 += -config.epsilon * db2
 
         # Assign new parameters to the model
-        model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+        model = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
 
         # Optionally print the loss.
         # This is expensive because it uses the whole dataset, so we don't want to do it too often.
         if print_loss and i % 1000 == 0:
-          print("Loss after iteration %i: %f" %(i, calculate_loss(model)))
+            print("Loss after iteration %i: %f" % (i, calculate_loss(model, X, y)))
 
     return model
 
-# %% 17
-# Build a model with a 3-dimensional hidden layer
-model = build_model(3, print_loss=True)
 
-# Plot the decision boundary
-plot_decision_boundary(lambda x: predict(model, x))
-plt.title("Decision Boundary for hidden layer size 3")
+def classify(X, y):
+    # clf = linear_model.LogisticRegressionCV()
+    # clf.fit(X, y)
+    # return clf
 
-# %% 14
-plt.figure(figsize=(16, 32))
-hidden_layer_dimensions = [1, 2, 3, 4, 5, 20, 50]
-for i, nn_hdim in enumerate(hidden_layer_dimensions):
-    plt.subplot(5, 2, i+1)
-    plt.title('Hidden Layer size %d' % nn_hdim)
-    model = build_model(nn_hdim)
-    plot_decision_boundary(lambda x: predict(model, x))
-plt.show()
+    pass
+
+
+def main():
+    X, y = generate_data()
+    config = Config()
+    model = build_model(X, y, config, 3, print_loss=True)
+    visualize(X, y, model)
+
+
+if __name__ == "__main__":
+    main()
---
+++ CAS-CS-640/programming1/q1.py
@@ -0,0 +1,23 @@
+## Linear Data
+# 1. Confusion matrix
+# 2. Precision
+# 3. Recall
+# 4. F-1 Score
+# 5. Accuracy
+# 6. Decision boundary decision boundary
+
+from nnet import *
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+import matplotlib
+
+# num_examples = len(X) # training set size
+# nn_input_dim = 2 # input layer dimensionality
+# nn_output_dim = 2 # output layer dimensionality
+#
+# # Gradient descent parameters (I picked these by hand)
+# epsilon = 0.01 # learning rate for gradient descent
+# reg_lambda = 0.01 # regularization strength
