353eea2295c05e3ed8f107f5a1d0d1c2b9b37efd
Alec Hoyland <alec.hoyland@posteo.net>
2019-11-15 11:45:39 -0500

    Add adaptive learning rate

--- CAS-CS-640/project/julia/denseAdpt.jl
+++ CAS-CS-640/project/julia/denseAdpt.jl
@@ -3,7 +3,7 @@ using Flux: onehotbatch, onecold, crossentropy, throttle
 using Base.Iterators: repeated
 # using CuArrays
 
-outfile = "mnist_dense.bson"
+outfile = "mnist_denseAdpt.bson"
 
 # Classify MNIST digits with a simple multi-layer-perceptron
 
@@ -31,7 +31,44 @@ opt = ADAM()
 ## Begin training
 
 @info("Beginning training loop...")
-training_time = @elapsed Flux.train!(loss, params(model), dataset, opt, cb = throttle(evalcb, 10))
+best_acc = 0.0
+last_improvement = 0
+training_time = @elapsed for epoch_idx in 1:100
+    global best_acc, last_improvement
+    # Train for a single epoch
+    Flux.train!(loss, params(model), dataset, opt)
+
+    # Calculate accuracy:
+    acc = accuracy(dataset...)
+    @info(@sprintf("[%d]: Test accuracy: %.4f", epoch_idx, acc))
+
+    # If our accuracy is good enough, quit out.
+    if acc >= 0.999
+        @info(" -> Early-exiting: We reached our target accuracy of 99.9%")
+        break
+    end
+
+    # If this is the best accuracy we've seen so far, save the model out
+    if acc >= best_acc
+        @info(" -> New best accuracy!")
+        best_acc = acc
+        last_improvement = epoch_idx
+    end
+
+    # If we haven't seen improvement in 5 epochs, drop our learning rate:
+    if epoch_idx - last_improvement >= 5 && opt.eta > 1e-6
+        opt.eta /= 10.0
+        @warn(" -> Haven't improved in a while, dropping learning rate to $(opt.eta)!")
+
+        # After dropping learning rate, give it a few epochs to improve
+        last_improvement = epoch_idx
+    end
+
+    if epoch_idx - last_improvement >= 10
+        @warn(" -> We're calling this converged.")
+        break
+    end
+end
 
 
 ## Compute the metrics
