5a031c4b1ddc9aeff0de62651d436a5d9e0f3ad3
Alec Hoyland <alec.hoyland@posteo.net>
2020-04-15 12:23:54 -0400

    Compartmentalize and try using environments

--- julia/Data-Science/introDataScience.jl
+++ julia/Data-Science/introDataScience.jl
@@ -1,18 +1,23 @@
 ## Julia for Data Science
 
+## Downloading Data
+
 # download the data
 cd("/home/alec/code/homework/julia/")
 P = download("https://raw.githubusercontent.com/nassarhuda/easy_data/master/programming_languages.csv", "programming_languages.csv")
 
+## Reading Data
+
 # read the data
 using CSV, DataFrames
-P = CSV.File("programming_languages.csv", header=true) |> DataFrame
+# P = CSV.File("programming_languages.csv", header=true) |> DataFrame
 # can also used DelimitedFiles.readdlm()
+P = CSV.read("programming_languages.csv", header=true)
 
 # when was language X created?
 function language_created_year(P::DataFrame, language::String)
-    loc = findfirst(P[2] .== language)
-    return P[loc,1]
+    loc = findfirst(P[!, 2] .== language)
+    return P[loc, 1]
 end
 
 # what year was Julia created?
@@ -21,7 +26,7 @@ language_created_year(P, "Julia")
 # create a more robust function that handles cases
 
 function language_created_year_v2(P::DataFrame, language::String)
-    loc = findfirst(lowercase.(P[2]) .== lowercase.(language))
+    loc = findfirst(lowercase.(P[!, 2]) .== lowercase.(language))
     return P[loc, 1]
 end
 
@@ -31,22 +36,32 @@ language_created_year_v2(P, "Julia") == language_created_year_v2(P, "julia")
 using DelimitedFiles
 writedlm("programming_languages_data.txt", Matrix(P), '-')
 P2 = readdlm("programming_languages_data.txt", '-') |> DataFrame
-P == P
+P == P2
+
+## Dictionaries
 
 # store the data in a Dictionary
 dict = Dict{Integer, Vector{String}}()
 
-for i = 1:size(Matrix(P),1)
-    year,lang = Matrix(P[i,:])
+for i = 1:size(P, 1)
+    year, lang = P[i, :];
+
     if year in keys(dict)
-        dict[year] = push!(dict[year],lang)
+        dict[year] = push!(dict[year], lang);
     else
-        dict[year] = [lang]
+        dict[year] = [lang];
     end
 end
 
 # what programming languages were developed in 2003
 dict[2003] # do it as a dictionary
+
+## Playing around with DataFrames
+
+# P is a data frame
+typeof(P)
+P[!, :year]
+
 using DataFramesMeta
 @linq P |> where(:year .== 2003) |> select(pl03 = :year, :language)
 
@@ -55,10 +70,11 @@ using DataFrames, Plots, CSV; gr()
 
 # load the data
 # download("http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv","houses.csv")
-houses = CSV.read("/home/alec/code/homework/julia/houses.csv")
+houses = CSV.read("/home/alec/code/homework/julia/Data-Science/houses.csv")
 
 # visualize the data
-scatter(houses[:sq__ft], houses[:price], markersize=3)
+scatter(houses, :sq__ft, :price, markersize=3, legend=false, xlabel="ft^2", ylabel="price (\$)")
+scatter(houses[!, :sq__ft], houses[!, :price], markersize=3, legend=false)
 
 # filter a data frame by feature value
 using Statistics
